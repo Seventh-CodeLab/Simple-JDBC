c5d9fd37e5ba8c7cf97f6b71fa0306aa6d8c36ac
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-10-09 17:02:59 +0200

    Altered from simple ArrayList to H2 Memory database

--- src/main/java/no/codelab/dao/ProductDao.java
+++ src/main/java/no/codelab/dao/ProductDao.java
@@ -1,16 +1,57 @@
 package no.codelab.dao;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
-  private List<String> products = new ArrayList<>();
+  private DataSource dataSource;
 
-  public void insertProduct(String product) {
-    this.products.add(product);
+  public ProductDao(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
-  public List<String> listAll() {
-    return products;
+  public void insertProduct(String productName) {
+    try (Connection connection = dataSource.getConnection()){
+      PreparedStatement statement = connection.prepareStatement(
+              "INSERT INTO products (name) values (?)"
+      );
+      statement.setString(1, productName);
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  public void insertProducts(String ...productNames) {
+    try (Connection connection = dataSource.getConnection()){
+      PreparedStatement statement = connection.prepareStatement(
+              "INSERT INTO products (name) values (?)"
+      );
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public List<String> listAll() throws SQLException {
+    try (Connection connection = dataSource.getConnection()) {
+      try (PreparedStatement statement = connection.prepareStatement
+              ("SELECT * FROM products"
+              )) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+          List<String> result = new ArrayList<>();
+
+          while (resultSet.next()){
+            result.add(resultSet.getString("name"));
+          }
+
+          return result;
+        }
+      }
+    }
   }
 }
--- src/test/java/no/codelab/dao/ProductDaoTest.java
+++ src/test/java/no/codelab/dao/ProductDaoTest.java
@@ -1,18 +1,26 @@
 package no.codelab.dao;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductDaoTest {
   @Test
-  void shouldRetrieveInsertedProduct(){
-    ProductDao dao = new ProductDao();
-    dao.insertProduct("White");
+  void shouldRetrieveInsertedProduct() throws SQLException {
+    JdbcDataSource dataSource = new JdbcDataSource();
+    dataSource.setUrl("jdbc:h2:mem:testBase"); //Type of Connection : DatabaseType : StorageLocation : DB Name
+    dataSource.getConnection().createStatement().executeUpdate(
+            "CREATE TABLE products (name varchar(100))"
+    );
+    ProductDao dao = new ProductDao(dataSource);
+    String productName = pickOne(new String[]{"White","Black","Gray","Light Gray", "Dark Gray"});
+    dao.insertProduct(productName);
     assertThat(dao.listAll())
-            .contains(pickOne(new String[]{"White","Black","Gray","Light Gray","Dark Gray"}));
+            .contains(productName);
   }
 
   private String pickOne(String[] products) {
